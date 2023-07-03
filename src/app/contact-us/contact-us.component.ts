a19d53708cadf51ccca3acde2026f11b1d3822bb
Mohd Nadeem <mohdnadeem.omnisttechub@gmail.com>
2023-07-03 10:35:33 +0530

    some changes in ecommodule

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.css']
 })
-export class ContactUsComponent {
+export class ContactUsComponent implements OnInit{
 
+  submitted:boolean = false;
+  constructor(){}
+  ngOnInit(): void {
+  }
+
+  contactUs = new FormGroup({
+    fname:new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(14),Validators.pattern('[ a-zA-Z]*')]),
+    lname:new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(14),Validators.pattern('[a-zA-Z]*')]),
+    email:new FormControl('',[Validators.required,Validators.email]),
+    addr:new FormControl('',[Validators.required]),
+    state:new FormControl('',[Validators.required,Validators.pattern('[ a-zA-Z]*')]),
+    phone:new FormControl('',[Validators.required,Validators.pattern('^(?:(?:\.))[0-9,]*$')])
+  });
+
+  onSubmit(data:any){
+    this.submitted = true;
+
+    if(this.contactUs.invalid){
+      return
+    }else{
+      console.log("Data Added Successfully",this.contactUs.value);
+      this.contactUs.reset();
+      this.submitted = false;
+    }
+  }
 }
